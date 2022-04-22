33836e76ae901509dab8b2971cac4ec74acd6e87
Jinsung Ha <jsung5381@naver.com>
2022-04-22 15:18:51 +0900

    Import gorilla websocket example

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,48 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "./frontend/index.html")
+}
+
+// https://github.com/gorilla/websocket/tree/master/examples/chat 옮기기
+
+/*
+	기반 기술 검증
+	1) client ↔ server websocket 통신 테스트
+	2) server와 redis 사이의 통신 테스트
+	3) 다중 server와 redis 사이의 통신 테스트
+*/
 func main() {
-	fmt.Println("started...")
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
