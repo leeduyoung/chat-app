7b6f439db8852f85e35dad590830fb3f891bc350
kaye <kaye.lee@luxrobo.com>
2022-04-22 17:20:02 +0900

    Test redis client connection for pub/sub

--- backend/main.go
+++ backend/main.go
@@ -5,9 +5,14 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
@@ -29,20 +34,67 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 
 /*
 	기반 기술 검증
-	1) client ↔ server websocket 통신 테스트
-	2) server와 redis 사이의 통신 테스트
-	3) 다중 server와 redis 사이의 통신 테스트
+	1) client ↔ server websocket 통신 테스트 - OK!
+	2) server와 redis 사이의 통신 테스트 -
+	3) 다중 server와 redis 사이의 통신 테스트 -
 */
 func main() {
-	flag.Parse()
-	hub := newHub()
-	go hub.run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)
+	var ctx = context.Background()
+
+	var redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "qwer1234",
 	})
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+
+	out := redisClient.Set(ctx, "kaye4", "3", 0)
+	fmt.Println("out", out)
+	if out.Err() != nil {
+		panic("??")
+	}
+
+	res := redisClient.Get(ctx, "kaye4")
+	fmt.Println("res", res)
+	if res.Err() != nil {
+		panic("???")
+	}
+
+	go subscribe(ctx, redisClient)
+
+	time.Sleep(time.Second * 2)
+
+	for i := 0; i < 5; i++ {
+		publish(ctx, redisClient)
+	}
+
+	// flag.Parse()
+	// hub := newHub()
+	// go hub.run()
+	// http.HandleFunc("/", serveHome)
+	// http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	// 	serveWs(hub, w, r)
+	// })
+	// err := http.ListenAndServe(*addr, nil)
+	// if err != nil {
+	// 	log.Fatal("ListenAndServe: ", err)
+	// }
+}
+
+func publish(ctx context.Context, redisClient *redis.Client) {
+	if err := redisClient.Publish(ctx, "test", "messgae-test").Err(); err != nil {
+		panic(err)
+	}
+}
+
+func subscribe(ctx context.Context, redisClient *redis.Client) {
+	subscriber := redisClient.Subscribe(ctx, "test")
+
+	for {
+		msg, err := subscriber.ReceiveMessage(ctx)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("msg: ", msg)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module chat-app
 
 go 1.16
 
-require github.com/gorilla/websocket v1.5.0
+require (
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/gorilla/websocket v1.5.0
+	gopkg.in/bufio.v1 v1.0.0-20140618132640-567b2bfa514e // indirect
+	gopkg.in/redis.v2 v2.3.2
+)
